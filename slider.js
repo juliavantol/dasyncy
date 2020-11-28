802cc6884084506e2c226442504e2672bd1be3a9
Julia van Tol <juliavantol@live.nl>
2020-11-28 22:41:51 +0100

    Movie and audio work again

--- slider.js
+++ slider.js
@@ -1,85 +1,83 @@
-var slider = document.getElementById("myRange");
-var hours = Math.floor(slider.value / 60);
-var minutes = slider.value % 60;
-var minuteOutput = $("#minutes")[0];
-var hourOutput = $("#hours")[0];
-var secOutput = $("#seconds")[0];
-var hours = Math.floor(slider.value / 60);
-var minutes = slider.value % 60;
-var seconds = slider.value % 60;
-//
-var slider2 = document.getElementById("myRange2");
-var hours2 = Math.floor(slider.value / 60);
-var minutes2 = slider.value % 60;
-var minuteOutput2 = $("#minutes2")[0];
-var hourOutput2 = $("#hours2")[0];
-var secOutput2 = $("#seconds2")[0];
+var hourOutput = document.getElementById("movieHours")
+var minuteOutput = document.getElementById("movieMinutes")
+var secOutput = document.getElementById("movieSeconds")
 
-var hours2 = Math.floor(slider.value / 60);
-var minutes2 = slider.value % 60;
-var seconds2 = slider.value % 60;
-hourOutput2.innerHTML = hours;
-minuteOutput2.innerHTML = minutes
-secOutput2.innerHTML = seconds
+var hourOutput2 = document.getElementById("audioHours")
+var minuteOutput2 = document.getElementById("audioMinutes")
+var secOutput2 = document.getElementById("audioSeconds")
 
-var movieSliderValue  = document.getElementById("movieValue");
-var audioSliderValue = document.getElementById("audioValue");
-
-hourOutput.innerHTML = hours;
-minuteOutput.innerHTML = minutes
-secOutput.innerHTML = seconds
+var movieSliderValue = document.getElementById("movieValue")
+var audioSliderValue = document.getElementById("audioValue")
 
 // movie
 function playback(){
-        var hours = Math.floor(slider.value / 3600);
-        var minutes = Math.floor((slider.value % 3600) / 60);
-        var seconds = slider.value % 60;
+        var hours = Math.floor(movieSlider.value / 3600);
+        var minutes = Math.floor((movieSlider.value % 3600) / 60);
+        var seconds = movieSlider.value % 60;
+
         hourOutput.innerHTML = hours;
         minuteOutput.innerHTML = minutes
         secOutput.innerHTML = seconds
+
         // audio
-        slider2.value = slider.value
+        audioSlider.value = movieSlider.value
         hourOutput2.innerHTML = hours;
         minuteOutput2.innerHTML = minutes
         secOutput2.innerHTML = seconds
 
-        movieSliderValue.innerHTML = slider.value
-        audioSliderValue.innerHTML = slider2.value
+        movieSliderValue.innerHTML = movieSlider.value
+        audioSliderValue.innerHTML = audioSlider.value
 
 }
 
 // audiotrack
 function playback2(){
-        var hours2 = Math.floor(slider2.value / 3600);
-        var minutes2 = Math.floor((slider2.value % 3600) / 60);
-        var seconds2 = slider2.value % 60;
+        var hours2 = Math.floor(audioSlider.value / 3600);
+        var minutes2 = Math.floor((audioSlider.value % 3600) / 60);
+        var seconds2 = audioSlider.value % 60;
         hourOutput2.innerHTML = hours2;
         minuteOutput2.innerHTML = minutes2
         secOutput2.innerHTML = seconds2
 
-
         // moviesync
-        slider.value = slider2.value
+        movieSlider.value = audioSlider.value
         hourOutput.innerHTML = hours2;
         minuteOutput.innerHTML = minutes2
         secOutput.innerHTML = seconds2
 
-        movieSliderValue.innerHTML = slider.value
-        audioSliderValue.innerHTML = slider2.value
+        movieSliderValue.innerHTML = movieSlider.value
+        audioSliderValue.innerHTML = audioSlider.value
 
 }
 
 function calculateValue() {
+     // Movie slider
+    var movieSlider = document.getElementById("movieSlider");
+    var hourOutput = $("#movieHours")[0];
+    var minuteOutput = $("#movieMinutes")[0];
+    var secOutput = $("#movieSeconds")[0];
+
+    // Audio slider
+    var audioSlider = document.getElementById("audioSlider");
+    var hourOutput2 = $("#audioHours")[0];
+    var minuteOutput2 = $("#audioMinutes")[0];
+    var secOutput2 = $("#audioSeconds")[0];
+
+    var movieSliderValue  = document.getElementById("movieValue");
+    var audioSliderValue = document.getElementById("audioValue");
+    var sliderContainer  = document.getElementById("sliderContainer");
+    var formContainer = document.getElementById("formContainer");
+
     var movietime = document.time.movie.value
     var audiotime = document.time.audio.value
-    var movielength = document.time.movielength.value
-    var audiolength = document.time.audiolength.value
+    var movielength = document.time.movieLength.value
+    var audiolength = document.time.audioLength.value
 
-    // hours
+    // Hours
     var h = movietime[0] + movietime[1]
-    // minutes
+    // Minutes
     var m = movietime[3] + movietime[4]
-    // seconds
+    // Seconds
     var s = movietime[6] + movietime[7]
 
     inth = parseInt(h, 10)
@@ -88,11 +86,14 @@ function calculateValue() {
     a = inth * 3600
     b = intm * 60
     c = a + b + ints
+    sliderContainer.removeAttribute("class", "hide");
+    formContainer.setAttribute("class", "hide");
+
 
 
 }
 
-document.getElementById("movieStart").value = "00:00:00";
-document.getElementById("audioStart").value = "00:00:00";
-document.getElementById("movieLength").value = "00:00:00";
-document.getElementById("audioLength").value = "00:00:00";
+$("#timeForm").submit(function(e) {
+
+    e.preventDefault();
+});
