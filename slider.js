ea4bc171aada692b100390ff55dc8e55dfb49f5d
Julia van Tol <juliavantol@live.nl>
2020-12-03 23:34:36 +0100

    Both sliders take input

--- slider.js
+++ slider.js
@@ -200,14 +200,7 @@ function playback2(){
 // Get the input field
 var input = document.getElementById("inputAudio");
 
-// Execute a function when the user releases a key on the keyboard
-input.addEventListener("keyup", function(event) {
-  // Number 13 is the "Enter" key on the keyboard
-  if (event.keyCode === 13) {
-    // Cancel the default action, if needed
-    alert("uolo")
-  }
-});
+
 
 
 $("#timeForm").submit(function(e) {
--- timestamp.js
+++ timestamp.js
@@ -1,43 +1,99 @@
 function moviespot() {
     // Get time
     var movieTime = document.getElementById("moviespotForm").value;
+    var audioTime = document.getElementById("audiospotForm").value;
 
-    if (movieTime.length < 8) {
-    // Divide the time
-    h = movieTime[0] + movieTime[1]
-    m = movieTime[3] + movieTime[4]
+    var movieSum = calculateLength(movieTime)
+    var audioSum = calculateLength(audioTime)
+
+    // Change spot in slider
+    var movieSlider = document.getElementById("movieSlider");
+    var audioSlider = document.getElementById("audioSlider");
+
+    var values = calculateValue()
+    movieStart = values[0]
+    audioStart = values[1]
+
+    var values2 = calculateDifference(audioStart, movieStart)
+    var difference = values2[0]
+    var msg = values2[1]
+    console.log(difference);
+
+    movieSlider.value = movieSum
+    // audiosync
+    if (msg == "audiofurther") {
+       audioSlider.value = (movieSum + difference)
+    } else {
+       audioSlider.value = (movieSum - difference)
+    }
+
+
+}
+
+
+
+function calculateLength(totalTime){
+
+    if (totalTime.length < 8) {
+    // Hours
+    var h = totalTime[0] + totalTime[1]
+    // Minutes < 5
+    var m = totalTime[3] + totalTime[4]
+
     inth = parseInt(h, 10)
     intm = parseInt(m, 10)
 
-    // Calculate spot
-    hours = inth * 3600
-    minutes = intm * 60
-    sum = (hours + minutes)
+    // Convert hours to seconds
+    a = inth * 3600
+    // Convert minutes to seconds
+    b = intm * 60
+    // Total amount of seconds, used to get the max value for the slider
+    maxValue = a + b
+    return maxValue;
 
     } else {
+    // Hours
+    var h = totalTime[0] + totalTime[1]
+    // Minutes < 5
+    var m = totalTime[3] + totalTime[4]
+    // Seconds < 8
+    var s = totalTime[6] + totalTime[7]
 
-    h = movieTime[0] + movieTime[1]
-    m = movieTime[3] + movieTime[4]
-    s = movieTime[6] + movieTime[7]
     inth = parseInt(h, 10)
     intm = parseInt(m, 10)
     ints = parseInt(s, 10)
 
-    // Calculate spot
-    hours = inth * 3600
-    minutes = intm * 60
-    sum = (hours + minutes + ints)
+    // Convert hours to seconds
+    a = inth * 3600
+    // Convert minutes to seconds
+    b = intm * 60
+    // Total amount of seconds, used to get the max value for the slider
+    maxValue = a + b + ints
+    return maxValue;
     }
-
-    console.log(sum);
-
-    // Change spot in slider
-    var movieSlider = document.getElementById("movieSlider");
-    movieSlider.value = sum
-    console.log(movieSlider.value);
 
 }
 
+function calculateDifference(audioStart, movieStart){
+
+     // Calculate difference
+    if (audioStart > movieStart) {
+        var difference = audioStart - movieStart
+        var msg = "audiofurther"
+
+    } else {
+        var difference = movieStart - audioStart
+        var msg = "moviefurther"
+    }
+    return [difference, msg];
+
+}
+
+
 $("#movieSpot").submit(function(e) {
+    e.preventDefault();
+});
+
+$("#audioSpot").submit(function(e) {
     e.preventDefault();
 });
